abffbee02b7134ade03be1a62b3b1933ec441e5a
Peter Ho <peter.ho@pushpay.com>
2020-04-17 16:54:08 +1200

    Updated JS module

--- index.d.ts
+++ index.d.ts
@@ -64,6 +64,7 @@ declare module 'react-native-google-cast' {
     launchExpandedControls(): void
     showIntroductoryOverlay(): void
     setVolume(volume: number): void
+    setPlaybackRate(rate: number): void
     initChannel(channel: string): Promise<boolean>
     sendMessage(message: string, namespace: string): Promise<boolean>
     showCastPicker(): void
